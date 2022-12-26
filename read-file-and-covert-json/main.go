f968cfe5f0488150600a8bb8efe9568833b51888
Zhenyu Shi <zhenyu.shi@compass.com>
2022-12-26 18:02:42 +0800

    Update: read a file and convert JSON to Go Struct

--- read-file-and-covert-json/main.go
+++ read-file-and-covert-json/main.go
@@ -1,18 +1,51 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
 )
 
-func main() {
+type Creature struct {
+	Name string     `json:"Name"`
+	Tags []string   `json:"Tags"`
+	HP   CreatureHP `json:"HP"`
+}
+
+type CreatureHP struct {
+	Value int    `json:"Value"`
+	Notes string `json:"Notes"`
+}
+
+func readDirectory() {
 	files, err := ioutil.ReadDir("./read-file-and-covert-json/data")
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	for _, file := range files {
-		fmt.Println(file.Name(), file.IsDir())
+		fmt.Printf("IsFile: %s\nIsFolder %t\n", file.Name(), file.IsDir())
 	}
+}
+
+func readNode() {
+	content, err := ioutil.ReadFile("./read-file-and-covert-json/data/1.json")
+	if err != nil {
+		fmt.Println("Error reading file")
+	}
+
+	var creature Creature
+	// golang的"显示哲学" + 指针传递
+	json.Unmarshal([]byte(content), &creature)
+
+	fmt.Println()
+	fmt.Println("Creature name: " + creature.Name)
+	fmt.Printf("Creature Tags: %q\n", creature.Tags)
+	fmt.Printf("Creature value: %+v Notes: %v\n", creature.HP.Value, creature.HP.Notes)
+}
+
+func main() {
+	readDirectory()
+	readNode()
 }
